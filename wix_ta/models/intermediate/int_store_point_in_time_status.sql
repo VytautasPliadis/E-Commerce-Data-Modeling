7310791068e7d62eb046bb2868a00f47dbb366cf
Vytautas Pliadis <vytautas.pliadis@gmail.com>
2024-07-23 18:53:17 +0300

    bug fix

--- wix_ta/models/intermediate/int_store_point_in_time_status.sql
+++ wix_ta/models/intermediate/int_store_point_in_time_status.sql
@@ -1,4 +1,7 @@
+{% set execution_date = var('execution_date') %}
 {% set execution_date_string = "'" ~ execution_date ~ "'" %}
 {% set execution_date_condition = "event_date <= " ~ execution_date_string %}
+
+{{ log(execution_date_condition, info=True) }}
 
 {{ generate_store_status(execution_date_condition) }}
